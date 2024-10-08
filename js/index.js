71df76c94bff148cc819cb2b67af1eb7885fd153
TioLindomar <mateusxavier0707@gmail.com>
2024-10-08 11:18:44 -0300

    NavBar concluida

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-document.querySelector('.ativar-menu').addEventListener('click', function() {
-    document.querySelector('.menu ul').classList.toggle('mostrar');
-});
+function AtivarMenu() {
+    const navbar_reduzida = document.querySelector('.navbar-reduzida')
+    navbar_reduzida.style.display = 'flex'
+    const tres_barras = document.querySelector('.ativar-menu')
+    tres_barras.style.zIndex = '-1'
+}
+
+function EsconderMenu() {
+    const navbar_reduzida = document.querySelector('.navbar-reduzida')
+    navbar_reduzida.style.display = 'none'
+    const tres_barras = document.querySelector('.ativar-menu')
+    tres_barras.style.zIndex = '1'
+}
