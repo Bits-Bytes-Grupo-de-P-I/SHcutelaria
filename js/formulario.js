cb945be4a48ca84630890afb3f367f5dd98efd2a
TioLindomar <mateusxavier0707@gmail.com>
2024-10-17 13:50:40 -0300

    Banners e página de formulário

--- js/formulario.js
+++ js/formulario.js
@@ -2,9 +2,20 @@ document.getElementById('submitBtn').addEventListener('click', function() {
     const selectedOption = document.querySelector('input[name="material"]:checked');
     
     if (selectedOption) {
-      document.getElementById('result').textContent = `Você selecionou: ${selectedOption.value}`;
+      document.getElementById('resultado').textContent = `Você selecionou: ${selectedOption.value}`;
     } else {
-      document.getElementById('result').textContent = "Nenhuma opção foi selecionada.";
+      document.getElementById('resultado').textContent = "Nenhuma opção foi selecionada.";
     }
   });
+  
+  // Limpar seleções dos botões radio
+  document.getElementById('resetBtn').addEventListener('click', function() {
+    document.getElementById('formulario-1').reset();
+    document.getElementById('formulario-2').reset();
+    document.getElementById('formulario-3').reset();
+    document.getElementById('formulario-4').reset();
+    document.getElementById('formulario-5').reset();
+    document.getElementById('formulario-6').reset();
+  });
+  
   
