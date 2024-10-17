d4483cd6b1f5f32395736d448111586f41851636
bernardowillian <bernardowillian13@gmail.com>
2024-10-17 16:42:30 -0300

    Adição de aviso sobre tamanho

--- js/blocos.js
+++ js/blocos.js
@@ -6,3 +6,17 @@ function toggleModal(modalId) {
         modal.style.display = "none";
     }
 }
+
+function toggleInfo() {
+    const infoMessage = document.getElementById('info-message');
+    infoMessage.style.display = 'block';
+
+    document.getElementById("info-icon").style.color = "#F05E1B";
+}
+
+function closeInfo() {
+    const infoMessage = document.getElementById('info-message');
+    infoMessage.style.display = 'none';
+
+    document.getElementById("info-icon").style.color = "#C0C0C0";
+}
