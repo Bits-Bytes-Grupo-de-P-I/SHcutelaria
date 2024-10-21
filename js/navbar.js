c630f14884a42635c6204db7c22e4daf48d3af58
TioLindomar <mateusxavier0707@gmail.com>
2024-10-21 11:00:31 -0300

    Organização dos arquivos

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,11 @@ function AtivarMenu() {
     const navbar_reduzida = document.querySelector('.navbar-reduzida')
     navbar_reduzida.style.display = 'flex'
     const tres_barras = document.querySelector('.ativar-menu')
+    const icone_x = document.querySelector('.fechar-menu')
     tres_barras.style.zIndex = '-1'
+    icone_x.style.display = 'flex'
+    
+
 }
 
 function EsconderMenu() {
