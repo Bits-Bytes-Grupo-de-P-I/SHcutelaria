d577bcc6e313c33076e88293894c74e24d8cc012
TioLindomar <mateusxavier0707@gmail.com>
2024-10-22 09:54:02 -0300

    Uatizapi

--- js/formulario.js
+++ js/formulario.js
@@ -1,3 +1,30 @@
+// Função para esconder todos os sub-formulários
+function hideAllSubForms() {
+  document.querySelector('.estilo-faca').style.display = 'none';
+  document.querySelector('.estilo-cutelo').style.display = 'none';
+  document.querySelector('.estilo-machado').style.display = 'none';
+}
+
+// Adiciona um listener ao formulário principal
+document.getElementById('formulario-1').addEventListener('change', (event) => {
+  // Verifica se o input alterado é do tipo radio
+  if (event.target.type === 'radio') {
+      // Esconde todos os sub-formulários
+      hideAllSubForms();
+
+      // Pega o valor da opção selecionada e mostra o sub-formulário correspondente
+      const selectedValue = event.target.nextElementSibling.querySelector('h3').textContent.toLowerCase();
+      const subFormToShow = document.querySelector(`.estilo-${selectedValue}`);
+      
+      if (subFormToShow) {
+          subFormToShow.style.display = 'block';
+      }
+  }
+});
+
+
+
+
 document.getElementById('submitBtn').addEventListener('click', function() {
     const selectedOption = document.querySelector('input[name="material"]:checked');
     
