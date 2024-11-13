e17a754acdceecda89301d4f0c1e12b90cf07402
TioLindomar <mateusxavier0707@gmail.com>
2024-11-13 12:54:17 -0300

    Correção do display da data na área "sorteio"

--- js/info_banco.js
+++ js/info_banco.js
@@ -33,7 +33,7 @@ window.onload = async function (e) {
         e.preventDefault();
 
         try {
-            const response = await fetch('https://sh-cutelaria.onrender.com/rifa/1', {
+            const response = await fetch('https://sh-cutelaria.onrender.com/rifa/3', {
                 method: 'GET',
             });
             const data = await response.json();
@@ -43,8 +43,12 @@ window.onload = async function (e) {
             document.getElementById('materialLamina').innerText = data.material_lamina;
             document.getElementById('materialCabo').innerText = data.material_cabo;
             document.getElementById('materialBainha').innerText = data.material_bainha;
-            document.getElementById('dataFinal').innerText = data.data;
-            
+            const dataInicial = data.data.split('T')[0]; // "2024-11-13"
+            const partesData = dataInicial.split('-'); // ["2024", "11", "13"]
+            const dataFormatada = `${partesData[2]}-${partesData[1]}-${partesData[0]}`; // "13-11-2024"
+            document.getElementById('dataFinal').innerText = dataFormatada;
+
+
         } catch (error) {
             alert(error.message);
         }
