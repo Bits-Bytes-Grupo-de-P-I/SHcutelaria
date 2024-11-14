d98242b4decb2bef26d1c823e9b77bd80d38a9d3
TioLindomar <mateusxavier0707@gmail.com>
2024-11-14 09:21:48 -0300

    Trechos de código do login comentados

--- js/info_banco.js
+++ js/info_banco.js
@@ -1,32 +1,3 @@
-// Função para ler o valor do cookie
-function getCookie(name) {
-    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-    console.log(match);
-    return match ? match[2] : null;
-}
-
-// Quando a página carregar, exibe o userName armazenado no cookie
-window.onload = function() {
-    const userName = getCookie('userName'); // Lê o valor do cookie 'userName'
-    console.log(getCookie('userName'));
-    
-    if (userName) {
-        document.getElementById('usuario').innerText = `Olá, ${userName}`;
-        document.getElementById('usuario-reduzido').innerText = `Olá, ${userName}`;
-        link = document.getElementById('link-login');
-        linkReduzido = document.getElementById('link-login-reduzido');
-        link.href = '#';
-        linkReduzido.href = '#'
-    } else {
-        document.getElementById('usuario').innerText = `Entrar`;
-        document.getElementById('usuario-reduzido').innerText = `Entrar`;
-        link = document.getElementById('link-login');
-        linkReduzido = document.getElementById('link-login-reduzido');
-        link.href = 'login.html';
-        linkReduzido.href = 'login.html';
-    }
-};
-
 
 // Função para pegar as informações do sorteio
 
